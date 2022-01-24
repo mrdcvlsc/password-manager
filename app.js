88b982c0c241a6e219db2fb4d7a7368962475a0e
mrdcvlsc <mordecaivelasco@gmail.com>
2022-01-24 15:37:32 +0800

    login page

---
+++ app.js
@@ -0,0 +1,17 @@
+const PORT = process.env.PORT || 8080;
+
+const path 		 = require('path');
+const fs 		 = require('fs');
+
+const express 	 = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+	res.sendFile(path.join(`${__dirname}/public/html/login.html`));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port : ${PORT}`);
+})

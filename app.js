514d4b47614acb357e7ee2d760125d48d07e2f31
mrdcvlsc <mrdcvlsc@gmail.com>
2022-06-28 16:09:42 +0800

    added more cases for displaying the HOSTs IP

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ fastify.register(require('./routes'));
 // start listening to port
 const start = async () => {
   try {
-    await fastify.listen(PORT, '::');
+    await fastify.listen({port: PORT, host:'::'});
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
@@ -27,11 +27,13 @@ start();
 
 // display device's network IP
 if(typeof(networkInterfaces.wlp2s0) !== 'undefined') {
-  console.log(`\n(a): app-server-ip: ${networkInterfaces.wlp2s0[0].address}:${PORT}\\\n\n`);
+  console.log(`\n(a) | app-server-ip: ${networkInterfaces.wlp2s0[0].address}:${PORT}\\\n\n`);
+} else if(typeof(networkInterfaces.enp3s0f1 !== 'undefined')) {
+  console.log(`\n(b) | app-server-ip: ${networkInterfaces.enp3s0f1[0].address}:${PORT}\\\n\n`);
 } else if(typeof(networkInterfaces['Wi-Fi'])!=='undefined') {
-  console.log(`\n(b): app-server-ip: ${networkInterfaces['Wi-Fi'][1].address}:${PORT}\\\n\n`);
+  console.log(`\n(c) | app-server-ip: ${networkInterfaces['Wi-Fi'][1].address}:${PORT}\\\n\n`);
 } else if(typeof(networkInterfaces.Ethernet) !== 'undefined') {
-  console.log(`\n(c): app-server-ip: ${networkInterfaces.Ethernet[1].address}:${PORT}\\\n\n`);
+  console.log(`\n(d) | app-server-ip: ${networkInterfaces.Ethernet[1].address}:${PORT}\\\n\n`);
 } else {
   console.log('\nno IP found for sharing over the network\n\n');
 }

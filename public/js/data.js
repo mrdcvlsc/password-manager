d352395e7327e99e14f4755b904c3f098a85d671
mrdcvlsc <mrdcvlsc@gmail.com>
2022-07-01 18:42:17 +0800

    render records in frontend

--- public/js/data.js
+++ public/js/data.js
@@ -1,28 +1,40 @@
 // Development Dummy Data
 let DummyTableRows = [
-  ['Instagram','@madoka16','instagram-password12345'],
-  ['Facebook','madoka.kadokawa','fb-secret-pass756'],
-  ['Gmail','madoka.magic@gmail.com','gpassword-yey'],
-  ['Twitter','@madoka_says','ultimatepassword777']
+  {username:'@madoka16',platform:'Instagram',password:'instagram-password12345'},
+  {username:'madoka.kadokawa',platform:'Facebook',password:'fb-secret-pass756'},
+  {username:'madoka.magic@gmail.com',platform:'Gmail',password:'gpassword-yey'},
+  {username:'@madoka_says',platform:'Twitter',password:'ultimatepassword777'}
 ];
 
 // Fill Table
 let TableBody = document.querySelector('tbody');
 function FillTable(data) {
   TableBody.innerHTML = '';
+
   for(let row of data) {
     console.log(row);
     let tr = document.createElement('tr');
-    tr.innerHTML = `<td class="selection"><input type="checkbox"></th>`;
-    for(let column of row) {
-      let td = document.createElement('td');
-      td.innerText = column;
-      tr.appendChild(td);
-    }
+    tr.innerHTML = `<td class="selection"><input type="checkbox"></td>`;
+
+    let platform = document.createElement('td');
+    let username = document.createElement('td');
+    let password = document.createElement('td');
+
+    platform.innerHTML = row.platform;
+    username.innerHTML = row.username;
+    password.innerHTML = row.password;
+
+    tr.appendChild(platform);
+    tr.appendChild(username);
+    tr.appendChild(password);
+    
     TableBody.appendChild(tr);
   }
 }
-FillTable(DummyTableRows);
+
+let response = await fetch('/records');
+let records = await response.json();
+FillTable(records);
 
 // Filters
 const SearchBar = document.querySelector('.search-bar');
